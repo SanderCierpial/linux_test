600620543592568e55ff25fb0cfb8ad9880fc92e
SanderCierpial <sanderoce@gmail.com>
2019-12-09 17:07:05 +0100

    dbfix2

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<?php include_once('/shared/nav.php') ?>
+<?php include_once('shared/nav.php') ?>
 
-<h1>index</h1>
+<h1>index fix</h1>
 
 <?php
-$db = new mysqli('localhost', 'sandercierpial', 'miVJbF2K8adR', 'sandercierpial_linuxproject', '33060');
+$db = new mysqli('localhost', 'homestead', 'secret', 'linux', '33060');
 
 if($db->connect_errno > 0) {
     die('Unable to connect to database [' . $db->connect_error . ']');
