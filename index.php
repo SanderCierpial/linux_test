f5babab9d14396c6eb06bc13f7c5c0a456fd268e
SanderCierpial <sanderoce@gmail.com>
2019-12-09 14:58:29 +0100

    testing

---
+++ index.php
@@ -0,0 +1,21 @@
+<h1>Docenten</h1>
+
+<?php
+$db = new mysqli('db.sinners.be', 'sandercierpial', 'miVJbF2K8adR', 'sandercierpial_linuxproject');
+
+if($db->connect_errno > 0) {
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$sql = 'SELECT * FROM docenten';
+
+if($result = $db->query($sql)){
+    while($row = $result->fetch_assoc()){
+        echo $row['name'] . ' ' . $row['surname'] . '<br />';
+    }
+}
+
+$result->free();
+$db->close();
+
+?>
