8cb52e0953e37e443b9f1128ae9008133ed4f0a9
SanderCierpial <sanderoce@gmail.com>
2019-12-09 20:37:27 +0100

    dbfix2sdfdfdd

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
-<h1>index fix</h1>
+<h1>index fix 2</h1>
 
 <?php
 $results = json_decode(file_get_contents('https://linuxdbapp.000webhostapp.com/getlinux.php'));
