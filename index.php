e8aad321b5edca7506a7c76dff7e354d0a44702c
SanderCierpial <sanderoce@gmail.com>
2019-12-09 16:54:58 +0100

    testingagainsdfsd

--- index.php
+++ index.php
@@ -19,10 +19,11 @@
 <h1>index</h1>
 
 <?php
-$db = new mysqli('sinners.be:22', 'sandercierpial', 'miVJbF2K8adR', 'sandercierpial_linuxproject');
+$db = new mysqli('localhost:3306', 'sandercierpial', 'miVJbF2K8adR', 'sandercierpial_linuxproject');
 
 if($db->connect_errno > 0) {
     die('Unable to connect to database [' . $db->connect_error . ']');
+    console.log("Unable to connect to database");
 }
 
 $sql = 'SELECT * FROM docenten';
